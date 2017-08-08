ca85a62d6872d9059e027d43daf141a2b2c862a0
Evan King <esk79@cornell.edu>
2017-08-07 17:22:46 -0700

    markdown table update

--- src/main/java/utils/MarkdownProcessor.java
+++ src/main/java/utils/MarkdownProcessor.java
@@ -5,19 +5,20 @@ package utils;
  */
 
 import com.vladsch.flexmark.ast.Node;
+import com.vladsch.flexmark.ext.gfm.strikethrough.StrikethroughExtension;
+import com.vladsch.flexmark.ext.tables.TablesExtension;
 import com.vladsch.flexmark.html.HtmlRenderer;
 import com.vladsch.flexmark.parser.Parser;
 import com.vladsch.flexmark.util.options.MutableDataSet;
+
+import java.util.Arrays;
 
 public class MarkdownProcessor {
     public String convertMarkdownToHTML(String markdown) {
         MutableDataSet options = new MutableDataSet();
 
-        // uncomment to set optional extensions
-        //options.set(Parser.EXTENSIONS, Arrays.asList(TablesExtension.create(), StrikethroughExtension.create()));
-
-        // uncomment to convert soft-breaks to hard breaks
-        //options.set(HtmlRenderer.SOFT_BREAK, "<br />\n");
+        // sets optional extensions
+        options.set(Parser.EXTENSIONS, Arrays.asList(TablesExtension.create(), StrikethroughExtension.create()));
 
         Parser parser = Parser.builder(options).build();
         HtmlRenderer renderer = HtmlRenderer.builder(options).build();
